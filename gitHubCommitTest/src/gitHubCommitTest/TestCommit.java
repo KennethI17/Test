b3373aa0018565dd77ef61748b24fad13479f397
irwin <irwin@LAPTOP-MUAD60K2>
2021-06-16 15:08:35 -0700

    2nd commit

--- gitHubCommitTest/src/gitHubCommitTest/TestCommit.java
+++ gitHubCommitTest/src/gitHubCommitTest/TestCommit.java
@@ -4,5 +4,6 @@ public class TestCommit {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello GitHub. Testing to see if i did this right");
+	System.out.println("Update code!");
 	}
 }
